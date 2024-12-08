26198c29678ecef3a560e262b81e23f1cb9d40a6
Nicolas-Cordero <nicolas.cordero01@alumnos.ucn.cl>
2024-12-08 05:28:23 -0300

    0.2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,67 @@
 #include "tablero.cpp"
 #include <iostream>
+#include <limits>
 using namespace std;
+
+int minimax(tablero &t, int profundidad, bool esMax, int alpha, int beta){
+    char jugador = esMax ? 'O' : 'X';
+    char oponente = esMax ? 'X' : 'O';
+    if(t.hayGanador() == oponente){
+        return -10 + profundidad;
+  
+    }
+    if(t.hayGanador() == jugador){
+        return 10 - profundidad;
+    }
+    if(t.tableroLleno()){
+        return 0;
+    }
+
+    int mejorPuntaje = esMax ? -10000 : 10000;
+
+    for (int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            if (t.getDato(i, j) == ' '){
+                t.hacerMovimiento(i, j, jugador);
+                int puntaje = minimax(t, profundidad + 1, !esMax, alpha, beta);
+                t.hacerMovimiento(i, j, ' ');
+
+                if (esMax){
+                    mejorPuntaje = max(puntaje, mejorPuntaje);
+                    alpha = max(alpha, puntaje);
+                }
+                else{
+                    mejorPuntaje = min(puntaje, mejorPuntaje);
+                    beta = min(beta, puntaje);
+                }
+                if (beta <= alpha){
+                    break;
+                }
+            }
+        }
+    }
+    return mejorPuntaje;
+}
+
+pair<int, int> obtenerMejorMovimiento(tablero &t){
+    int mejorPuntaje = -10000; 
+    pair<int, int> mejorMovimiento = {-1,-1};
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            if (t.getDato(i, j) == ' '){
+                t.hacerMovimiento(i, j, t.getOponente());
+                int puntaje = minimax(t, 0 , false, -10000, 10000);
+                t.hacerMovimiento(i, j, ' ');
+
+                if (puntaje > mejorPuntaje){
+                    mejorPuntaje = puntaje;
+                    mejorMovimiento = {i, j};
+                }
+            }
+        }
+    }
+    return mejorMovimiento;
+}
 
 int conversorSimpleLetra(char letra){
     if (letra == 'A' || letra == 'a'){
@@ -28,9 +89,9 @@ int pvp(){
         cin >> fila;
         
 
-        if (t.hacerMovimiento(fila, columna)){
+        if (t.hacerMovimiento(fila, columna, ' ')){
             t.imprimirTablero();
-            if (t.hayGanador()){
+            if (t.hayGanador() != ' '){
                 cout << "Jugador " << t.getJugadorActual() << " ha ganado!" << endl;
                 break;
             }
@@ -39,6 +100,12 @@ int pvp(){
                 break;
             }
             t.cambiarJugador();
+            cout << "--------------------------------" << endl;
+            auto mejorMovimiento = obtenerMejorMovimiento(t);
+            cout << "siguiente mejor movimiento";
+            cout << "first: " << mejorMovimiento.first;
+            cout << "second: " << mejorMovimiento.second;
+            cout << "--------------------------------" << endl;
         }
         else{
             cout << "Movimiento invalido, intente de nuevo" << endl;
--- tablero.cpp
+++ tablero.cpp
@@ -6,10 +6,17 @@ class tablero{
     private:
         vector<vector<char>> matriz;
         char jugadorActual;
+        char oponente; 
     public:
         tablero(){
             matriz = vector<vector<char>>(3, vector<char>(3, ' '));
             jugadorActual = 'X';
+        }
+        char getDato(int fila, int columna){
+            return matriz[fila][columna];
+        }
+        int getSize(){
+            return matriz.size();
         }
         void imprimirTablero(){ /// funcion 1, imprimir tablero
             cout << "    A   B   C" << endl;
@@ -25,26 +32,35 @@ class tablero{
                 cout << "  +---+---+---+" << endl;
             }
         }
-        bool hacerMovimiento(int fila, int columna){ /// funcion 2, hacer movimiento
+        bool hacerMovimiento(int fila, int columna, char jugadorForzado){ /// funcion 2, hacer movimiento
             if (fila >= 0 && fila < 3 && columna >= 0 && columna < 3 && matriz[fila][columna] == ' '){
+                if (jugadorForzado != ' '){
+                    matriz[fila][columna] = jugadorForzado;
+                    return true;
+                }
                 matriz[fila][columna] = jugadorActual;
                 return true;
             }
             return false;
         }
-        bool hayGanador(){ /// funcion 3, hay ganador
+        char hayGanador(){ /// funcion 3, hay ganador
             for (int i = 0; i < 3; ++i){
-                // verificamos filas y columnas
-                if ((matriz[i][0] == jugadorActual && matriz[i][1] == jugadorActual && matriz[i][2] == jugadorActual)
-                 || (matriz[0][i] == jugadorActual && matriz[1][i] == jugadorActual && matriz[2][i] == jugadorActual)){
-                    return true;
+              
+                if ((matriz[i][0] == jugadorActual && matriz[i][1] == jugadorActual && matriz[i][2] == jugadorActual) ||
+                (matriz[0][i] == jugadorActual && matriz[1][i] == jugadorActual && matriz[2][i] == jugadorActual)){
+                    return jugadorActual;
                 }
-                // verificamos diagonales
-                return ((matriz[0][0] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][2] == jugadorActual)
-                 || (matriz[0][2] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][0] == jugadorActual));
-                
+
+                else if ((matriz[0][0] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][2] == jugadorActual) ||
+                (matriz[0][2] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][0] == jugadorActual)){
+                    return jugadorActual;
+                }
             }
-            
+            if ((matriz[0][0] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][2] == jugadorActual) ||
+                (matriz[0][2] == jugadorActual && matriz[1][1] == jugadorActual && matriz[2][0] == jugadorActual)) {
+                return jugadorActual;
+            }
+            return ' ';
         }
         bool tableroLleno(){ /// funcion 4, tablero lleno
             for (int i = 0; i < 3; i++){
@@ -58,9 +74,18 @@ class tablero{
         }
         void cambiarJugador(){ /// funcion 5, cambiar jugador
             jugadorActual = (jugadorActual == 'X') ? 'O' : 'X';
+            if (jugadorActual == 'X'){
+                oponente = 'O';
+            }
+            else{
+                oponente = 'X';
+            }
         }
         char getJugadorActual(){ /// funcion 6, obtener jugador actual
             return jugadorActual;
+        }
+        char getOponente(){ /// funcion 6.1, obtener oponente
+            return oponente;
         }
         void reiniciarTablero(){ /// funcion 7, reiniciar tablero
             matriz = vector<vector<char>>(3, vector<char>(3, ' '));
